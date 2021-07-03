e1b88e79e1e468dc95377f1b13fb07eb1a627886
gilles-bertrand <gilles@triptyk.eu>
2021-07-03 16:54:49 +0200

    add port

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ server.db = router.db;
 server.use(middlewares);
 server.use(auth)
 server.use(router);
-server.listen(3004, () => {
+server.listen(process.env.port || 3004, () => {
     console.log('JSON SERVER is running')
 })
